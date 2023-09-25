e3bd13d38d54fdb500c160c7569ccdd91262442f
Ali <ali@infracost.io>
2023-09-25 18:05:14 +0300

    feat: bump instance type

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+  instance_type = "m5.8xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   tags = {
     Environment = "production"
@@ -22,8 +22,8 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 500
-    iops        = 800
+    volume_size = 5000
+    iops        = 1000
   }
 }
 
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "hello_world" {
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+  memory_size   = 512                      # <<<<< Try changing this to 512 to compare costs
 
   tags = {
     Environment = "Prod"
