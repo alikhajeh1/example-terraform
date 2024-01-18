69f3647af86909f2b047e90b4d2b03ddec881ba1
Ali Khajeh-Hosseini <ali@infracost.io>
2024-01-18 12:10:33 -0800

    Create infracost_test.tf

---
+++ infracost_test.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web_app3" {
+  ami           = "ami-674cbc1e"
+  instance_type = "m3.xlarge"
+
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
+
+  root_block_device {
+    volume_size = 50
+  }
+}
