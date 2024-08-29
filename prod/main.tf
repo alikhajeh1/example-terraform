d14799e2dc24628e91c43c0165283c53fa9cfe58
Ali Khajeh-Hosseini <ali@infracost.io>
2024-08-29 14:23:21 -0700

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -45,7 +45,7 @@ resource "aws_s3_bucket" "another-example1" {
 resource "aws_instance" "new_web_app" {
   ami           = "ami-005e54dee72cc1d00"
 
-  instance_type = "m5.4xlarge"
+  instance_type = "m3.4xlarge"
 
   tags = {
     Environment = "production"
